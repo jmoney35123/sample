f4715c1c41526c97abdbfe12ada236036c51813d
jmoney35123 <javonwilliams786@gmail.com>
2018-11-07 14:27:30 -0500

    created main

---
+++ src/main/java/person/Main.java
@@ -0,0 +1,13 @@
+package person;
+
+public class Main {
+    public static void main(String[] args) {
+        String javon = "javon";
+        Person person= new Person();
+        person.setName(javon);
+        System.out.println(person.getName());
+
+
+
+    }
+}
